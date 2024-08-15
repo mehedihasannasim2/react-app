6f1dc10fc22d5c075e4df26e276537a345a747f0
nasim-etlimited <nasim@etlimited.net>
2024-08-15 09:55:16 +0600

    adding a Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
+import Card from './Card.jsx'
 
 
 function App() {
   return(
     <>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
       <Header/>
       <Food/>
       <Footer/>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,13 @@
+import profilePic from './assets/person.jpg'
 
-// function Card(){
-//     return()
-// }
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" src={profilePic} alt="profile picture" />
+            <h2 className='card-title'> Brooo </h2>
+            <p className='card-text'> He likes to sleep, eat & chill. He doesn't care about life. </p>
+        </div>
+    )
+}
 
-// export default Card
+export default Card
